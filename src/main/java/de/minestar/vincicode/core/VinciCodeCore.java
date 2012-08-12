831a78ed0e6cefe16422af2bd52cade0c16315d0
Meldanor <Bhaals@gmx.de>
2012-08-12 22:22:44 +0200

    Initial commit
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/vincicode/core/VinciCodeCore.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of VinciCode.
+ * 
+ * VinciCode is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * VinciCode is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with VinciCode.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.vincicode.core;
+
+import de.minestar.minestarlibrary.AbstractCore;
+
+public class VinciCodeCore extends AbstractCore {
+
+    public static final String NAME = "VinciCode";
+
+    public VinciCodeCore() {
+        super(NAME);
+    }
+}
